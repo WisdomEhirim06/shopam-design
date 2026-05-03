5ac257d61ca22cdfe72283e3ab322db3697e3f68
Wisdom Ehirim <erichenrywisel@gmail.com>
2026-05-03 22:23:13 +0100

    fix: productcategory added to productservice

--- lib/api/types.ts
+++ lib/api/types.ts
@@ -96,6 +96,7 @@ export interface ProductService {
   owner_name: string;
   title: string;
   description?: string;
+  category?: string;
   price: string;
   tax_inclusive: boolean;
   item_type: ItemType;
