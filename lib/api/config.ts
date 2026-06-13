10ac7e21b5e860be4a724656e5992e55e2eb0714
osayanhu <jerryosaimo@gmail.com>
2026-06-13 23:43:58 +0100

    gave proper url

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -159,7 +159,7 @@ export const API_ENDPOINTS = {
     VERIFY_EMAIL: '/api/accounts/user-verify',
     TOKEN_REFRESH: '/api/accounts/token/refresh',
     VENDOR_PROFILE_UPDATE: '/api/accounts/path/vendor-update',
-    VENDOR_UPGRADE: '/api/accounts/register/vendor-upgrade',
+    VENDOR_UPGRADE: '/api/accounts/register/existing-vendor',
   },
 
   // Products
